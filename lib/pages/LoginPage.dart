a26efe19d9cbe20d635a149e0f350d9a03d292bf
Adarsh Dwivedi <moon@Adarsh-MacBook-Pro.local>
2023-04-30 02:11:39 +0530

    StatefulWidget, setState(), custom button (container), InkWell, Animated Container, await, async

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:my_intern/utils/Routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String demoText = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -16,7 +24,7 @@ class LoginPage extends StatelessWidget {
               //use it instead of padding
               SizedBox(height: 20),
 
-              const Text("Welcome",
+               Text("Welcome $demoText",
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
 
               Padding(
@@ -30,6 +38,10 @@ class LoginPage extends StatelessWidget {
                     Padding(
                       padding: const EdgeInsets.symmetric(vertical:30 ),
                       child: TextFormField(
+                        onChanged: (txt){
+                          demoText = txt;
+                          setState(() {});
+                        },
                         obscureText: true,
                         decoration: InputDecoration(
                             hintText: "Enter Password",
@@ -39,13 +51,44 @@ class LoginPage extends StatelessWidget {
                     ),
                     SizedBox(height: 20),
 
-                    ElevatedButton(
-                        onPressed:(){
-                          print("Button is pressed");
-                          Navigator.pushNamed(context, MyRoutes.homeRoute);
-                        },
-                        style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                        child: Text("Login"))
+                    // ElevatedButton(
+                    //     onPressed:(){
+                    //       print("Button is pressed");
+                    //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //     },
+                    //     style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    //     child: Text("Login"))
+                    InkWell(
+                      onTap: () async{
+                        setState(() {
+                          changeButton = changeButton? false: true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
+
+                        changeButton? Navigator.pushNamed(context, MyRoutes.homeRoute) : null;
+                      },
+                      child: AnimatedContainer(
+                        width: 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: changeButton? Colors.blue : Colors.purple,
+                          shape: BoxShape.rectangle,
+                          //borderRadius: BorderRadius.circular(50)
+                        ),
+                        duration: Duration(seconds: 1),
+
+                        child: changeButton? Icon(Icons.done) : Text(
+                          "Login",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontFamily: GoogleFonts.aboreto().fontFamily,
+                            fontSize: 16
+                          ),
+                        ),
+                      ),
+                    )
                   ],
                 ),
               )
