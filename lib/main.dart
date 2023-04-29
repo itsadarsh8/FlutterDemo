6e0c93a37718330ea8602bc4ced0b0076988fc4c
Adarsh Dwivedi <moon@Adarsh-MacBook-Pro.local>
2023-04-29 21:42:24 +0530

    MaterialApp, Material, Scaffold, theme, routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:my_intern/Home_Page.dart';
+import 'package:my_intern/pages/LoginPage.dart';
+import '/pages/HomePage.dart';
 
-void main(){
+void main() {
   runApp(myInterns());
 }
 
 class myInterns extends StatelessWidget {
-  const myInterns({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Home_Page(),
+      //use this or "/" in routes
+      //home:HomePage(),
+
+      //choose theme type here
+      themeMode: ThemeMode.light,
+
+      //customise light theme
+      theme: ThemeData(primarySwatch: Colors.blue, brightness: Brightness.light),
+
+      //customise dark theme
+      darkTheme: ThemeData(brightness: Brightness.dark, primarySwatch: Colors.red),
+
+      //customise first route
+      initialRoute: "/home",
+
+      //Layout manager
+      routes: {
+        "/": (context) => new HomePage(),
+        "/login": (context) => new LoginPage(),
+      },
     );
   }
 }
-
---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String time = "4:20";
+  var months = "24 months";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Interns: App"),
+      ),
+      body: Center(
+          child: Container(
+              child: Text("days = $days , time = $time , months = $months ",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.blue
+                  )))),
+      drawer: Drawer(),
+    );
+  }
+}
